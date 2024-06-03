4252153c84f380e775ad860e094c0b41f0ab0b83
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2024-06-03 15:06:00 +0700

    commit 10 iput outfile.close untuk selesai menulis dan menutup file, ifstream untuk membuka file dalam mode membaca, infile.open untuk menunjuk ke sebuah file dan cout yg menampilkan "membuka dan membaca file"

--- IOfileWithPath.cpp
+++ IOfileWithPath.cpp
@@ -27,4 +27,14 @@ int main() {
         //menulis dan memasukkan nilai dari 'baris' ke dalam file 
         outfile << baris << endl;
     }
+    //selesai jika menulis sekarang tutup filenya
+    outfile.close();
+
+    //membuka file dalam mode membaca
+    ifstream infile;
+
+    //menunjuk ke sebuah file
+    infile.open(NamaFile + ".txt", ios::in);
+
+    cout << endl << ">= membuka dan membaca file" << endl;
 }
