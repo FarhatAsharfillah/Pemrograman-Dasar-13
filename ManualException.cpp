b816fbfb8fb8521c9130a5e2b6b415941331f5c1
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2024-06-03 15:15:42 +0700

    commit 11 input int main dengan try dengan cout "selamat belajar di ti umy" dan cout "pernyataan tidak dapat dieksekusi"

---
+++ ManualException.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    try {
+        cout << "selamat belajar di prodi ti UMY" << endl;
+        //throw 0.5; //melemparkan sebuah interger maka
+        cout << "pernyataan tidak akan dieksekusi" << endl;
+    }
+}
