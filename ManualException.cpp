b64f837c78e316d19febe49522a386575ec74e6c
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2024-06-03 15:19:49 +0700

    commit 13 input 2 catch untuk blok akan dieksekusi dengan cout dan catch untuk jika selain interger maka akan dieksekusi dengan cout

--- ManualException.cpp
+++ ManualException.cpp
@@ -8,4 +8,14 @@ int main()
         //throw 0.5; //melemparkan sebuah interger maka
         cout << "pernyataan tidak akan dieksekusi" << endl;
     }
+    catch (int a) {
+        //blok ini akan dieksekusi
+        cout << "pengecualian akan dieksekusi" << endl;
+    }
+    catch (...) {
+        /*jika selain interger maka block ini akan dieksekusi*/
+        cout << "default pengecualian dieksekusi" << endl;
+    }
+
+    return 0;
 }
